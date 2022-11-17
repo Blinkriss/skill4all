78b1f6ed1134cb1739b7151252cd143bfe111120
Blinkriss <mom.bonna@gmail.com>
2022-11-17 10:03:48 +0100

    project setup

---
+++ src/Entity/Car.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CarRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CarRepository::class)
+ */
+class Car
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbSeats;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbDoors;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $cost;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=CarCategory::class, inversedBy="cars")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $carCategory;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNbSeats(): ?int
+    {
+        return $this->nbSeats;
+    }
+
+    public function setNbSeats(int $nbSeats): self
+    {
+        $this->nbSeats = $nbSeats;
+
+        return $this;
+    }
+
+    public function getNbDoors(): ?int
+    {
+        return $this->nbDoors;
+    }
+
+    public function setNbDoors(int $nbDoors): self
+    {
+        $this->nbDoors = $nbDoors;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCost(): ?int
+    {
+        return $this->cost;
+    }
+
+    public function setCost(int $cost): self
+    {
+        $this->cost = $cost;
+
+        return $this;
+    }
+
+    public function getCarCategory(): ?CarCategory
+    {
+        return $this->carCategory;
+    }
+
+    public function setCarCategory(?CarCategory $carCategory): self
+    {
+        $this->carCategory = $carCategory;
+
+        return $this;
+    }
+}
---
+++ src/Entity/CarCategory.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CarCategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CarCategoryRepository::class)
+ */
+class CarCategory
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Car::class, mappedBy="carCategory")
+     */
+    private $cars;
+
+    public function __construct()
+    {
+        $this->cars = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Car>
+     */
+    public function getCars(): Collection
+    {
+        return $this->cars;
+    }
+
+    public function addCar(Car $car): self
+    {
+        if (!$this->cars->contains($car)) {
+            $this->cars[] = $car;
+            $car->setCarCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCar(Car $car): self
+    {
+        if ($this->cars->removeElement($car)) {
+            // set the owning side to null (unless already changed)
+            if ($car->getCarCategory() === $this) {
+                $car->setCarCategory(null);
+            }
+        }
+
+        return $this;
+    }
+}
