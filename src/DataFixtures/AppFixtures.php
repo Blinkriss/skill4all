92f73220c38f1506940fce95f258a854802212b6
Blinkriss <mom.bonna@gmail.com>
2022-11-17 22:52:00 +0100

    add provider car to Faker + populate fake data

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,38 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use App\Entity\Car;
+use App\Entity\CarCategory;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create();
+        $faker->addProvider(new \Faker\Provider\Car($faker));
+
+        for ($cc = 0; $cc < 5; $cc++) {
+            $carCategory = new CarCategory;
+
+            $carCategory->setName($faker->carCategory());
+
+            $manager->persist($carCategory);
+
+            for ($c = 0; $c < mt_rand(15, 20); $c++) {
+                $car = new Car;
+
+                $car->setName($faker->car())
+                    ->setNbSeats($faker->numberBetween(1, 8))
+                    ->setNbDoors($faker->numberBetween(2, 10))
+                    ->setCost($faker->randomNumber(5, true))
+                    ->setCarCategory($carCategory);
+
+                $manager->persist($car);
+            }
+        }
 
         $manager->flush();
     }
